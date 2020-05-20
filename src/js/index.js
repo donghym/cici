4d2975d6a37d3597d8753c5fb3a567ab92d6c804
donghym <donghym@yonyou.com>
2020-05-20 15:26:33 +0800

    模板绑定html

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 function hello(str){
     console.log(str)
+    console.log(22)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
-
-// 
-module.exports = {
+const htmlWebpackPlugin = require("html-webpack-plugin")
+ let config = {
     "entry":{
-        a:__dirname+'/src/js/main.js',
-        b:__dirname+'/src/js/index.js'
+        "a":__dirname+'/src/js/main.js',
+        "b":__dirname+'/src/js/index.js',
+        "c":__dirname+'/src/js/index2.js'
     },
     "output":{
-        "path":__dirname+"/dist/js/",
-        "filename":"[name].[chunkhash].js"
+        "path":__dirname+"/dist",
+        "filename":"js/[name].[chunkhash].js"
+    },
+    "plugins":[
+        new htmlWebpackPlugin({
+            "filename":"index.html",
+            "template":"index.html",
+            "inject":"head",
+            "title":"dhy",
+            "date":new Date()
+        })
+    ]
+}
+
+
+module.exports =(env,args)=>{
+    console.log(env);
+    console.log(args);
+    if(args.mode !=="development"){
+        config.output.publicPath="http://cdn.com/"
     }
+    return config
 }
